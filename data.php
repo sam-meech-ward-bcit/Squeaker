97de4a56f72f4530dc4d774f179c86ad47eec579
Sam Meech-Ward <sam@meech-ward.com>
2019-10-07 11:19:05 -0700

    Update data.php

--- data.php
+++ data.php
@@ -1,41 +1,27 @@
 <?php
 
-// returns an associative array of all of the squeaks
-function getAllSqueaks() {
-  $string = file_get_contents("./data.json");
-  return json_decode($string, true)["squeaks"];
-}
 
 // returns a single associative array squeak
 function getSqueak($squeakId) {
-  return getAllSqueaks()[$squeakId];
+
 }
 
-// saves an associative array containing every single squeak
-function saveAllSqueaks($squeaks) {
-  $fp = fopen('./data.json', 'w');
-  fwrite($fp, json_encode(["squeaks" => $squeaks]));
-  fclose($fp);
+
+// returns an associative array of all of the squeaks
+function getAllSqueaks($username, $order) {
+  
 }
 
 // saves a new squeak to the database
-function saveNewSqueak($squeak) {
-  $squeaks = getAllSqueaks();
-  $squeaks[$squeak["id"]] = $squeak;
-  saveAllSqueaks($squeaks);
-  return $squeak;
-}
-
-// updates an existing squeak in the database
-function updateSqueak($squeak) {
-  $squeaks = getAllSqueaks();
-  $squeaks[$squeak["id"]] = $squeak;
-  saveAllSqueaks($squeaks);
+function saveNewSqueak($message, $username) {
+  
 }
 
 // deletes a single squeak
 function deleteSqueak($squeakId) {
-  $squeaks = getAllSqueaks();
-  unset($squeaks[$squeakId]);
-  saveAllSqueaks($squeaks);
+}
+
+// increments the like count of a single squeak
+function incrementLikeCount($squeakId, $increment) {
+
 }
